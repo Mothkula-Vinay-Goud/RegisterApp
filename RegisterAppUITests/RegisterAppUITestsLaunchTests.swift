d9d102d15c7e468355f78f3ac9fbb050476a1022
Vinay <mothkulavinaygoud@gmail.com>
2026-01-18 00:48:17 -0600

    Initial Commit

---
+++ RegisterAppUITests/RegisterAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RegisterAppUITestsLaunchTests.swift
+//  RegisterAppUITests
+//
+//  Created by Vinay Goud Mothkula on 1/18/26.
+//
+
+import XCTest
+
+final class RegisterAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
