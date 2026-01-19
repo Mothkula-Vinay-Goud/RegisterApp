a9572653c7a2ded6da203267d3132970b8eac47a
Vinay <mothkulavinaygoud@gmail.com>
2026-01-19 14:54:38 -0600

    A project to add two numbers

--- RegisterApp/ViewController.swift
+++ RegisterApp/ViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
 
+    
+    @IBOutlet weak var firstNumberTF: UITextField!
+    @IBOutlet weak var secondNumberTF: UITextField!
+    
+    @IBOutlet weak var resultSum: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        let first = Int(firstNumberTF.text ?? "") ?? 0
+        let second = Int(secondNumberTF.text ?? "") ?? 0
+        let sum = first + second
+        resultSum.text = "\(sum)"
+    }
+    
+
+    @IBAction func clearClicked(_ sender: Any) {
+        firstNumberTF.text = ""
+        secondNumberTF.text = ""
+        resultSum.text = "Result is Displayed Here"
+    }
+    
 
 
 }
